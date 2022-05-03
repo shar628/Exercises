d4a51e694aed04765ee62ff3100e4113602749b0
shar628 <shar628628@gmail.com>
2022-05-03 11:08:00 +0300

    7.3

---
+++ javascriptpart1/7.1.7.2Array Methods/app.7.2.js
@@ -0,0 +1,4 @@
+const string = "The more you know, the more you know that you don't know";
+
+// לא הצלחתי 
+
---
+++ javascriptpart1/7.1.7.2Array Methods/app.7.3.js
@@ -0,0 +1,6 @@
+const fruits = ["Banana", "Orange", "Lemon", "Apple", "Mango"];
+
+// const fruits2 = fruits.slice(0, -2);
+const fruits2 = fruits.slice(1, -2);
+
+console.log(fruits2);
