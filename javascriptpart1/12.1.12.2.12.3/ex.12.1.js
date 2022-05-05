db6a73220120820b952f6caabc020117d7704af7
shar628 <shar628628@gmail.com>
2022-05-05 22:00:51 +0300

    12.1

--- javascriptpart1/12.1.12.2.12.3/ex.12.1.js
+++ javascriptpart1/12.1.12.2.12.3/ex.12.1.js
@@ -18,8 +18,18 @@ var library = [
 // לעבור על המערך בעיקר על הסטטוס ,לבדוק אם הוא טרו,
 //  אם כן לקחת את הנתונים שלו,
 //  לחשוב אולי לתת שמות לאובייקטים של המערך כדי לגשת אליהם בקלות 
-const Book1 = function book2(params) {
-    library.forEach(readingStatus === 1)
-    console.log("ddd");
+// const Book1 = function book2(params) {
+//     library.forEach(readingStatus === 1)
+//     console.log("ddd");
+// }
+// console.log(library.author);
+function book3(library) {
+    return library
+        .filter((i) => {
+            return i.readingStatus === true;
+        })
+        .map((i) => {
+            return `${i.author},${i.title},${i.readingStatus} `
+        })
 }
-console.log(library.author);
+console.log(book3(library));
