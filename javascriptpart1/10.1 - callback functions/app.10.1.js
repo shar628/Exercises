f7700d298ae273c1f34f65a9f2e6c2f69a57b5e8
shar628 <shar628628@gmail.com>
2022-05-05 14:47:48 +0300

    10.1 10.2

--- javascriptpart1/10.1 - callback functions/app.10.1.js
+++ javascriptpart1/10.1 - callback functions/app.10.1.js
@@ -3,34 +3,37 @@ const isString = function (str, callback) {
         callback(str);
     }
 }
-function callback2(str2) {
-    console.log(str2);
+function callback2(str4) {
+    // console.log(str4);
 };
 
 // isString("shaul", callback2);
 // isString("5555", callback2);
 // isString(5555, callback2);
 // 2.
+// let str5 = "who is the big?"
 const firstWordUpperCase = function (string1, callback1) {
     let arr1 = string1.split(" ");
     arr1[0] = arr1[0].toUpperCase();
     callback1(arr1.join(" "));
+    console.log(arr1);
 };
+// firstWordUpperCase(str5, callback2)
 const addDash = (string1) => {
     let str3 = "";
-
 }
+
 // from free code camp
 function createQuote(quote, callback) {
     var myQuote = "Like I always say, " + quote;
     callback(myQuote); // 2
 }
 
-function logQuote(quote) {
-    console.log(quote);
+function logQuote(ar) {
+    console.log(ar);
 }
 
-createQuote("eat your vegetables!", logQuote); // 1
+// createQuote("eat your vegetables!", logQuote); // 1
 
 // Result in console:
 // Like I always say, eat your vegetables!
@@ -44,4 +47,26 @@ function fun5(num5) {
     return num5;
     console.log(num5);
 }
-console.log(fun4(20, fun5));
+// console.log(fun4(20, fun5));
+// fun4(25, fun5);
+
+// ========
+
+let str9 = ' i love isreal';
+let func1 = (str, callback5) => {
+    const [fir, ...rest] = str.split(" ");
+    const res1 = [fir.toUpperCase(), ...rest].join(" ");
+    if (typeof callback5 === "function") {
+        return callback5(res1);
+    } else {
+        return;
+    }
+};
+const str8 = (s) => {
+    return s.split(" ").join("-");
+}
+const str7 = (st) => {
+    return st + " $$";
+}
+console.log(func1(str9, str8));
+console.log(func1(str9, str7));
--- javascriptpart1/10.1 - callback functions/tyuta.js
+++ javascriptpart1/10.1 - callback functions/tyuta.js
@@ -1,4 +1,20 @@
 
 
 const myObj = { firstName: 'Henry', lastName: 'Williams' };
-console.log('myObj', Object.values(myObj));
+// console.log('myObj', Object.values(myObj));
+
+let arr1 = [4, 5, 8, 's', 'a', 'w'];
+// console.log(arr1);
+arr1.push('q');
+// console.log(arr1);
+arr1.unshift('sh');
+let sera = arr1.indexOf('a');
+let del_ar = arr1.splice(sera, 1)
+console.log(arr1);
+console.log(del_ar);
+
+window.onload = function () {
+    console.log('Hello 5.5.22 10:41');
+    console.log(del_ar);
+    document.querySelector('#id_h3').innerHTML = arr1;
+}
