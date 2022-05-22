9cc9fc353bcdcabd08eab652d5eb5e27fa2a7634
shar628 <shar628628@gmail.com>
2022-05-23 02:36:15 +0300

    2.2

--- react/12.1-chuck_norris/my-app/src/App.js
+++ react/12.1-chuck_norris/my-app/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import React from 'react';
 import EX2_1 from './exersizes/ex.2.1';
+import EX2_2 from './exersizes/ex.2.2';
 
 function App() {
   return (
     <div className="App">
-      <EX2_1 />
+      {/* <EX2_1 /> */}
+      <EX2_2 />
     </div>
   );
 }
