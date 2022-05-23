80aa65088303a1ef3805fee764f7a08c34c52517
shar628 <shar628628@gmail.com>
2022-05-23 03:04:12 +0300

    4.1

--- react/12.1-chuck_norris/my-app/src/App.js
+++ react/12.1-chuck_norris/my-app/src/App.js
@@ -3,13 +3,15 @@ import React from 'react';
 import EX2_1 from './exersizes/ex.2.1';
 import EX2_2 from './exersizes/ex.2.2';
 import Box1 from './exersizes/ex.3.1';
+import Ex4_1_buttons_props from './exersizes/ex.4.1';
 
 function App() {
   return (
     <div className="App">
       {/* <EX2_1 /> */}
       {/* <EX2_2 /> */}
-      <Box1 />
+      {/* <Box1 /> */}
+      <Ex4_1_buttons_props />
     </div>
   );
 }
---
+++ react/12.1-chuck_norris/my-app/src/exersizes/ex.4.1.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+class Ex4_1_buttons_props extends React.Component {
+    render() {
+        return (
+            <div>
+                <ButtonComp text="important" />
+                <ButtonComp text="not important" />
+            </div>
+        );
+    }
+}
+
+const ButtonComp = (props) => {
+    return <button className="button1">{props.text}</button>;
+};
+
+export default Ex4_1_buttons_props;
