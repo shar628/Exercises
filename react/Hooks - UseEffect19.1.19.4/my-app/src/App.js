d9babe5909fa4e66288fceced0e18002169b865d
shar628 <shar628628@gmail.com>
2022-05-31 00:21:48 +0300

    19.1 v1

--- react/Hooks - UseEffect19.1.19.4/my-app/src/App.js
+++ react/Hooks - UseEffect19.1.19.4/my-app/src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Ex_19_3 from './components/ex.19.3';
+import Ex_19_1 from './components/ex.19.1';
 
 function App() {
   return (
     <div className="App">
-      <Ex_19_3 />
+      {/* <Ex_19_3 /> */}
+      <Ex_19_1 />
+
     </div>
   );
 }
---
+++ react/Hooks - UseEffect19.1.19.4/my-app/src/components/ex.19.1.jsx
@@ -0,0 +1,27 @@
+import React, { useEffect } from 'react'
+import axios from 'axios'
+import { useState } from 'react'
+
+function Ex_19_1() {
+    const [data, setData]= useState([])
+
+useEffect(()=>{
+const fetchData = async ()=>{
+    try{
+        const { data} = await axios.get(
+`https://pipedream.com/apps/swapi`
+        );
+console.log(data);
+    }catch(e){
+        console.log(e);
+    }
+}
+fetchData()
+}, [])
+
+  return (
+    <div>Ex_19_1</div>
+  )
+}
+
+export default Ex_19_1
